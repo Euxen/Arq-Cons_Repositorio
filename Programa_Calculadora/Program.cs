1660de0009d77c1ca5c6c65c5c77272e929eb9a3
Euxen <kelvinhezhang@live.com>
2022-02-24 03:01:26 -0400

    Add project files.

---
+++ Programa_Calculadora/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Saludos, favor digitar un numero");
+var input = Console.ReadLine();
+
+// We need to confirm that it is a number
+var validNumber = int.TryParse(input, out var value);
+
+// We need a way to end this loop
+if (validNumber == false)
+{
+    Console.WriteLine("Favor entrar un numero valido! \n");
+}
+else
+{
+    //Here we start doing all the calculations required
+    Console.Clear();
+    Console.WriteLine("Nota: Los siguentes valores seran redondeados a numeros enteros.\n");
+
+    //First is the absolute value of the input
+    Console.WriteLine($"El valor absoluto de {input} es {Math.Abs(value)} \n");
+    //Second is the 2nd power of the input
+    Console.WriteLine($"La potencia quadrada de {input} es { Math.Pow(value, 2)}\n");
+    //Third is the square root of the input
+    Console.WriteLine($"La raiz quadrada de {input} es {Math.Round(Math.Sqrt(value)), 2}\n");
+    //Fourth is the sine value of the input
+    Console.WriteLine($"El valor seno de {input} es {Math.Round(Math.Sin(value)), 2}\n");
+    //Fifth is the cosine value of the input
+    Console.WriteLine($"El valor coseno de {input} es {Math.Round(Math.Cos(value)), 2} \n");
+
+    //Esto nos ayuda prevenir que la consola se cierre cuando se termine las calculaciones
+    Console.Write($"{Environment.NewLine} Presione cualquier tecla para salir");
+    Console.ReadKey(true);
+}
+
+
---
+++ Programa_ConversionTemperatura/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Saludos, favor digitar una temperatura en Fahrenheit");
+var input = Console.ReadLine();
+
+// We need to confirm that it is a number
+var validNumber = decimal.TryParse(input, out var value);
+
+// We need a way to end this loop
+if (validNumber == false)
+{
+    Console.WriteLine("Favor entrar una temperatura valida! \n");
+}
+else
+{
+    //Here we start doing all the calculations required
+    Console.Clear();
+
+    //From Fahrenheit to Celsius
+    var Celsius = Math.Round((value - 32) * 5 / 9, 2);
+    Console.WriteLine($"La temperatura en Fahrenheit es de {value}F, su equivalente en Celsius en de {Celsius}C");
+
+    //From Celius to Fahrenheit
+    Console.WriteLine($"La temperatura en Celsius es de {Celsius}C, su equivalente en Celsius en de {value}F");
+
+    //Prevent the console from closing the moment the program is done
+    Console.Write($"{Environment.NewLine} Presione cualquier tecla para salir");
+    Console.ReadKey(true);
+}
